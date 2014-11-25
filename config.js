0f2173ff377e6397a26e783d33863aedba2d5fa0
Markuz GJ <markuz.br@gmail.com>
2014-11-24 21:17:00 -0800

    refactor build

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  src: './index.js',
+  standalone: 'BundleNamespace.runtime',
+  sourcemaps: true,
+  entry:'./index.js',
+  basename: 'runtime-namespace',
+  dest: '.',
+  title: 'runtime',
+}
