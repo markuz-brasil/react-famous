a5ebceac0d9b07446dc742540b1dd8427e8986ca
Markuz GJ <markuz.br@gmail.com>
2014-11-24 18:03:31 -0800

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+import 'setimmediate'
+import 'es6-shim'
+import 'regenerator/runtime'
+
+import * as React from 'react/addons'
+import * as PropTypes from 'react/lib/ReactPropTypes'
+import * as CSSPropertyOperations from 'react/lib/CSSPropertyOperations'
+import * as ReactTransitionChildMapping from 'react/lib/ReactTransitionChildMapping'
+
+import * as di from 'di'
+import * as Famous from 'famous'
+
+React.lib = {
+  PropTypes: PropTypes,
+  CSSPropertyOperations: CSSPropertyOperations,
+  ReactTransitionChildMapping: ReactTransitionChildMapping,
+}
+
+export { DI, Famous, React}
---
+++ specs.js
@@ -0,0 +1,5 @@
+
+// just placeholder for specs
+
+
+
