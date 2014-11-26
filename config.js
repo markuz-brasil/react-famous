b4578c70cbc60a92b23bf4d1fe88a0c278f579a2
Markuz GJ <markuz.br@gmail.com>
2014-11-25 20:23:33 -0800

    refactored config

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  src: './index.js',
   standalone: 'BundleNamespace.runtime',
   sourcemaps: true,
   entry:'./index.js',
   basename: 'runtime-namespace',
   dest: './build',
   title: 'runtime',
+  js: {
+    src:  [
+      './index.{js,jsx}',
+      './src/**/*.{js,jsx}',
+    ],
+    opt: {},
+  },
 }
